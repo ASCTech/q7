edbc10bc840eb86da11a27a1c5cead2866445cd0
Mike Butsko <butsko.7@osu.edu>
2012-04-17 13:02:51 -0400

    Starting on styles.

---
+++ templates/zone--user.tpl.php
@@ -0,0 +1,6 @@
+<?php if ($wrapper): ?><div<?php print $attributes; ?>><?php endif; ?>  
+  <div<?php print $content_attributes; ?>>    
+    TESTETSTETETST
+    <?php print $content; ?>
+  </div>
+<?php if ($wrapper): ?></div><?php endif; ?>
