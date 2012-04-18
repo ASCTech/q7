e7fb7d128e91dda0ae5ffd7267beff1d77895ab5
Mike Butsko <butsko.7@osu.edu>
2012-04-18 09:59:25 -0400

    New approach to osulogo

--- templates/section--header.tpl.php
+++ templates/section--header.tpl.php
@@ -11,7 +11,7 @@
           <a title="The Ohio State University homepage" href="http://www.osu.edu/">The Ohio State University</a>
         </p>
         <p id="site-name">
-           Site Name
+        &gt; Arts and Sciences &gt; <?php print $site_name; ?>
         </p>
       </div>
       <div id="osu-NavbarLinks">
