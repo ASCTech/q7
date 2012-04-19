20bea805cf5969898c51909e99424262fbe6e34f
Mike Butsko <butsko.7@osu.edu>
2012-04-19 12:25:17 -0400

    Color settings

--- theme-settings.php
+++ theme-settings.php
@@ -3,7 +3,7 @@
 /**
  * Implements hook_form_system_theme_settings_alter()
  */
-function omega_form_system_theme_settings_alter(&$form, &$form_state) {
+function q7_form_system_theme_settings_alter(&$form, &$form_state) {
   $theme = alpha_get_theme();
 
   $form['theme_settings']['asc_brand'] = array(
@@ -11,14 +11,20 @@ function omega_form_system_theme_settings_alter(&$form, &$form_state) {
     '#title' => t('Include ASC Branding'),
     '#default_value' => theme_get_setting('asc_brand'),
     );
+
+  $form['theme_settings']['asc_header_left_color'] = array(
+    '#type' => 'textfield',
+    '#size' => 7,
+    '#maxlength' => 7,
+    '#title' => t('Header Left Color'),
+    '#default_value' => them_get_settings('asc_header_left_color'),
+  );
   
-  $form['quickSites_header_right_color'] = array(
+  $form['theme_settings']['asc_header_right_color'] = array(
     '#type' => 'textfield',
     '#size' => 7,
     '#maxlength' => 7,
     '#title' => t('Header Right Color'),
-    '#default_value' => $settings['quickSites_header_right_color'],
-    '#prefix' => '<div style="float: left; width: 140px;">',
-    '#suffix' => '</div>',
+    '#default_value' => them_get_settings('asc_header_right_color'),
   );
 }
