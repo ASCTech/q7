b84e20bfdae49350a133675621a2be079db4fee9
Mike Butsko <butsko.7@osu.edu>
2012-04-20 15:23:16 -0400

    Still working

--- templates/zone--preface.tpl.php
+++ templates/zone--preface.tpl.php
@@ -1,8 +1,8 @@
-<div id="header-tile">&nbsp;</div>
 <?php if ($wrapper): ?><div<?php print $attributes; ?>><?php endif; ?>  
+<div id="header-tile">&nbsp;</div>
   <div<?php print $content_attributes; ?>>    
     <?php if ($is_front): ?>
-      <div class="banner-container"><a id="ascbanner" href="http://asc.osu.edu/">&nbsp;</a></div>
+      <div id="banner-container"><a id="ascbanner" href="http://asc.osu.edu/">&nbsp;</a></div>
     <?php endif; ?>    
     <?php print $content; ?>
   </div>
