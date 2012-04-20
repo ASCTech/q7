859c94a72f2662da97bcbb0824f43906609ca0ad
Mike Butsko <butsko.7@osu.edu>
2012-04-20 15:11:14 -0400

    Preface tile

--- templates/zone--preface.tpl.php
+++ templates/zone--preface.tpl.php
@@ -1,8 +1,8 @@
 <?php if ($wrapper): ?><div<?php print $attributes; ?>><?php endif; ?>  
   <div<?php print $content_attributes; ?>>    
     <?php if ($is_front): ?>
-      <div class="banner-container"><a id="ascbanner" href="http://asc.osu.edu/">&nbsp;</a></div>
-      <div class="header-tile">&nbsp;</a>
+      <div id="banner-container"><a id="ascbanner" href="http://asc.osu.edu/">&nbsp;</a></div>
+      <div id="header-tile">&nbsp;</a></div>
     <?php endif; ?>    
     <?php print $content; ?>
   </div>
