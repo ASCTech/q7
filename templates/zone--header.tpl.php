519bcd33496518369c8af298432de77071d3eadf
Mike Butsko <butsko.7@osu.edu>
2012-04-20 15:53:10 -0400

    test

--- templates/zone--header.tpl.php
+++ templates/zone--header.tpl.php
@@ -1,9 +1,11 @@
 <?php if ($wrapper): ?><div<?php print $attributes; ?> style="position: relative;"><?php endif; ?>  
     <?php if ($is_front): ?>
       <div id="header-tile">&nbsp;</div>
-      <div id="banner-container"><a id="ascbanner" href="http://asc.osu.edu/">&nbsp;</a></div>
     <?php endif; ?>    
   <div<?php print $content_attributes; ?>>    
+    <?php if ($is_front): ?>
+      <div id="banner-container"><a id="ascbanner" href="http://asc.osu.edu/">&nbsp;</a></div>
+    <?php endif; ?>    
     <?php print $content; ?>
   </div>
 <?php if ($wrapper): ?></div><?php endif; ?>
