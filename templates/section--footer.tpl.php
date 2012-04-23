61f9e1d45d2fdcc00dffac868a9f78f0b1f7f162
Mike Butsko <butsko.7@osu.edu>
2012-04-23 16:00:37 -0400

    Copyright

--- templates/section--footer.tpl.php
+++ templates/section--footer.tpl.php
@@ -1,3 +1,9 @@
 <footer<?php print $attributes; ?>>
   <?php print $content; ?>
+  <div class="container-12">
+    <div id="copyright" class="grid-12">
+    &copy; 2012, The Ohio State University, Designed by 
+    <a href="http://asccomm.osu.edu" title="ASC Communications website">Arts and Sciences Communications Services</a>.
+    <a href="/user">Login</a>
+  </div>
 </footer>
