3f427aeb0ca8e9d67ba8b65da1849406b3997c9c
Mike Butsko <butsko.7@osu.edu>
2012-04-23 12:45:56 -0400

    bottom flag position

--- templates/zone--footer.tpl.php
+++ templates/zone--footer.tpl.php
@@ -1,5 +1,5 @@
-<?php if ($wrapper): ?><div<?php print $attributes; ?> style="position: relative;"><?php endif; ?>  
-  <div<?php print $content_attributes; ?>>    
+<?php if ($wrapper): ?><div<?php print $attributes; ?>><?php endif; ?>  
+  <div<?php print $content_attributes; ?> style="position: relative">    
     <?php if ($is_front): ?>
       <div id="footer-banner-container"><a id="ascfooterbanner" href="http://asc.osu.edu/">&nbsp;</a></div>
     <?php endif; ?>    
