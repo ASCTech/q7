1bc34b6d9541fddfa82c06d71048ce6dab73e75e
Mike Butsko <butsko.7@osu.edu>
2012-04-23 07:56:23 -0400

    Footer banner

--- templates/section--footer.tpl.php
+++ templates/section--footer.tpl.php
@@ -1,3 +1,6 @@
 <footer<?php print $attributes; ?>>
+    <?php if ($is_front): ?>
+      <div id="footer-banner-container"><a id="ascfooterbanner" href="http://asc.osu.edu/">&nbsp;</a></div>
+    <?php endif; ?>    
   <?php print $content; ?>
 </footer>
