8ca0de23d22c7662f5fee51524611a3ddab4cec4
Mike Butsko <butsko.7@osu.edu>
2012-04-30 10:38:44 -0400

    Header tile has a new div in town.

--- templates/zone--header.tpl.php
+++ templates/zone--header.tpl.php
@@ -1,6 +1,6 @@
 <?php if ($wrapper): ?><div<?php print $attributes; ?> style="position: relative;"><?php endif; ?>  
     <?php if ($is_front): ?>
-      <div id="header-tile">&nbsp;</div>
+      <div id="header-tile"><div id="header-tile-bar">&nbsp;</div></div>
     <?php endif; ?>    
   <div<?php print $content_attributes; ?>>    
     <?php if ($is_front): ?>
