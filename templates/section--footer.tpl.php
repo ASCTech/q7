5222359d1dd7c352d606cf7a8e5a7ff76f817733
Mike Butsko <butsko.7@osu.edu>
2012-07-20 14:18:20 -0400

    Fix Login URL

--- templates/section--footer.tpl.php
+++ templates/section--footer.tpl.php
@@ -4,6 +4,6 @@
     <div id="copyright" class="grid-12">
     &copy; 2012, The Ohio State University, Designed by 
     <a href="http://asccomm.osu.edu" title="ASC Communications website">Arts and Sciences Communications Services</a>.
-    <a href="/user">Login</a>
+    <a href="/Shibboleth.sso/Login?target=<?php print urlencode("https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']) ?>">Login</a>
   </div>
 </footer>
