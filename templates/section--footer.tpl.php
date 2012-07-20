f108c249038fe7b51a3abfe254cc747694d5012e
Mike Butsko <butsko.7@osu.edu>
2012-07-20 14:23:33 -0400

    Logout

--- templates/section--footer.tpl.php
+++ templates/section--footer.tpl.php
@@ -4,7 +4,7 @@
     <div id="copyright" class="grid-12">
     &copy; 2012, The Ohio State University, Designed by 
     <a href="http://asccomm.osu.edu" title="ASC Communications website">Arts and Sciences Communications Services</a>.
-    <?php if(!user_is_logged_in()) { ?> }
+    <?php if(!user_is_logged_in()) { ?> 
       <a href="/Shibboleth.sso/Login?target=<?php print urlencode("https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']) ?>" title="Site Administrator Login">Login</a>
     <?php } else { ?>
       <a href="/user/logout">Log Out</a>
