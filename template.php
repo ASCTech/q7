eca891c90c5df3429e06987080d55cd81124f245
Mike Butsko <butsko.7@osu.edu>
2012-08-06 15:25:01 -0400

    Misc updates, remove all day label.

--- template.php
+++ template.php
@@ -5,7 +5,7 @@
  * This file is empty by default because the base theme chain (Alpha & Omega) provides
  * all the basic functionality. However, in case you wish to customize the output that Drupal
  * generates through Alpha & Omega this file is a good place to do so.
- * 
+ *
  * Alpha comes with a neat solution for keeping this file as clean as possible while the code
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
@@ -35,4 +35,11 @@ function q7_process_page(&$variables, $hook) {
 
 function q7_preprocess_html(&$variables) {
   drupal_add_css('https://fonts.googleapis.com/css?family=Oxygen', array('type' => 'external'));
-} 
+}
+
+function q7_date_all_day_label() {
+/** This overrides a theme found in the date module so that "All Day" will not display on the calendar.
+To return to normal function replace the line with return t('(All day)'); or any other text that you would rather display
+for an all day event. Might be better to override the whole them function rather than the label but this is an easy fix **/
+  return t('');
+}
