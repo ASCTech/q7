3ee4426b02cb5b3e5ba8d6552f1ece56647f614e
Mike Butsko <butsko.7@osu.edu>
2012-08-07 12:07:47 -0400

    Theme settings

---
+++ theme-settings.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ *
+ */
+
+function q7_settings($saved_settings) {
+    $settings = theme_get_settings('q7');
+
+  $form['sublogo'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Logo'),
+    '#description' => t("May be displayed behind primary logo.")
+  );
+  $form['sublogo']['use_sublogo'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Use a background for the logo.'),
+    '#default_value' => $settings['use_sublogo'],
+  );
+  $form['sublogo']['sublogo_path'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Path to logo background'),
+    '#default_value' => $settings['sublogo_path'],
+  );
+
+  $form['sublogo']['sublogo_upload'] = array(
+    '#type' => 'file',
+    '#title' => t('Upload logo background image'),
+  );
+  $form['#submit'][] = 'q7_settings_submit';
+  $form['sublogo']['sublogo_upload']['#element_validate'][] = 'q7_settings_submit';
+
+  return $form;
+}
+
+/**
+ * Capture theme settings submissions and update uploaded image
+ */
+function q7_settings_submit($form, &$form_state) {
+  // Check for a new uploaded file, and use that if available.
+  if ($file = file_save_upload('sublogo_upload')) {
+    $parts = pathinfo($file->filename);
+    $filename = (! empty($key)) ? str_replace('/', '_', $key) .'_sublogo.'. $parts['extension'] : 'sublogo.'. $parts['extension'];
+
+    // The image was saved using file_save_upload() and was added to the
+    // files table as a temporary file. We'll make a copy and let the garbage
+    // collector delete the original upload.
+    if (file_copy($file, $filename)) {
+      $_POST['use_sublogo'] = $form_state['values']['use_sublogo'] = TRUE;
+      $_POST['sublogo_path'] = $form_state['values']['sublogo_path'] = $file->filepath;
+    }
+  }
+}
