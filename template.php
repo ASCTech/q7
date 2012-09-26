509185dc5f767cd27bca67cc6247a57a991fa087
Mike Butsko <butsko.7@osu.edu>
2012-09-26 08:34:56 -0400

    Override calendar title format

--- template.php
+++ template.php
@@ -43,3 +43,51 @@ To return to normal function replace the line with return t('(All day)'); or any
 for an all day event. Might be better to override the whole them function rather than the label but this is an easy fix **/
   return t('');
 }
+
+function get_current_filters() {
+  $url_array = explode('/', drupal_get_path_alias());
+  $terms = array_pop($url_array);
+  $filter = array_pop($url_array);
+  if ($filter == 'filter') {
+    return '/filter/' . $terms;
+  }
+}
+
+function q7_date_nav_title($params) {
+  $granularity = $params['granularity'];
+  $view = $params['view'];
+  $date_info = $view->date_info;
+  $link = !empty($params['link']) ? $params['link'] : FALSE;
+  $format = !empty($params['format']) ? $params['format'] : NULL;
+  switch ($granularity) {
+    case 'year':
+      $title = $date_info->year;
+      $date_arg = $date_info->year;
+      break;
+    case 'month':
+      //MB Changed.
+      $format = !empty($format) ? $format : 'F Y');
+      $title = date_format_date($date_info->min_date, 'custom', $format);
+      $date_arg = $date_info->year . '-' . date_pad($date_info->month);
+      break;
+    case 'day':
+      $format = !empty($format) ? $format : (empty($date_info->mini) ? 'l, F j, Y' : 'l, F j');
+      $title = date_format_date($date_info->min_date, 'custom', $format);
+      $date_arg = $date_info->year . '-' . date_pad($date_info->month) . '-' . date_pad($date_info->day);
+      break;
+    case 'week':
+      $format = !empty($format) ? $format : (empty($date_info->mini) ? 'F j, Y' : 'F j');
+      $title = t('Week of @date', array('@date' => date_format_date($date_info->min_date, 'custom', $format)));
+      $date_arg = $date_info->year . '-W' . date_pad($date_info->week);
+      break;
+  }
+  if (!empty($date_info->mini) || $link) {
+    // Month navigation titles are used as links in the mini view.
+    $attributes = array('title' => t('View full page month'));
+    $url = date_pager_url($view, $granularity, $date_arg, TRUE) . get_current_filters();
+    return l($title, $url, array('attributes' => $attributes));
+  }
+  else {
+    return $title;
+  }
+}
