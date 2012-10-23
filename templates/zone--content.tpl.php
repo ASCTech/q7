0e641f97d72f8c31e8c86e6fc834c892afa1e77e
Mike Butsko <butsko.7@osu.edu>
2012-10-23 10:50:53 -0400

    new templates

---
+++ templates/zone--content.tpl.php
@@ -0,0 +1,18 @@
+<?php if ($wrapper): ?><div<?php print $attributes; ?>><?php endif; ?>  
+  <div<?php print $content_attributes; ?>>    
+    <?php if ($breadcrumb): ?>
+      <div class="breadcrumb-wrapper" class="container-12">
+      <div class="grid-8 breadcrumb-left"> 
+        <?php print $breadcrumb; ?>
+      </div>
+      <div class="grid-4 breadcrumb-right">&nbsp;</div>
+      </div>
+    <?php endif; ?>    
+<!--
+    <?php if ($messages): ?>
+      <div id="messages" class="grid-<?php print $columns; ?>"><?php print $messages; ?></div>
+    <?php endif; ?>
+-->
+    <?php print $content; ?>
+  </div>
+<?php if ($wrapper): ?></div><?php endif; ?>
