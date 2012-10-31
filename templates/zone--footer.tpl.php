bbd41c7be4ea966157dacea5132b72682193bfb9
Mike Butsko <butsko.7@osu.edu>
2012-10-31 15:25:32 -0400

    Branding option, current username in footer

--- templates/zone--footer.tpl.php
+++ templates/zone--footer.tpl.php
@@ -1,6 +1,6 @@
 <?php if ($wrapper): ?><div<?php print $attributes; ?>><?php endif; ?>  
   <div<?php print $content_attributes; ?> style="position: relative">    
-    <?php if ($is_front && (get_variable('q7_asc_branding') != 1): ?>
+    <?php if ($is_front && (get_variable('q7_asc_branding')) != 1) : ?>
       <div id="footer-banner-container"><a id="ascfooterbanner" href="http://asc.osu.edu/"></a></div>
     <?php endif; ?>    
     <?php print $content; ?>
--- templates/zone--header.tpl.php
+++ templates/zone--header.tpl.php
@@ -3,7 +3,7 @@
       <div id="header-tile"><div id="header-tile-bar">&nbsp;</div></div>
     <?php endif; ?>    
   <div<?php print $content_attributes; ?>>    
-    <?php if ($is_front)  && (get_variable('q7_remove_asc_branding') != 1): ?>
+    <?php if (($is_front) && (get_variable('q7_remove_asc_branding') != 1)) : ?>
       <div id="banner-container"><a id="ascbanner" href="http://asc.osu.edu/"></a></div>
     <?php endif; ?>    
     <?php print $content; ?>
