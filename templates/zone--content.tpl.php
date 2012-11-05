289cd134b78f72377bb170504a6c5c05f3daf0ff
Mike Butsko <butsko.7@osu.edu>
2012-11-05 08:37:21 -0500

    Sidebar color modify, and breadcrumb right bar updates too.

--- templates/zone--content.tpl.php
+++ templates/zone--content.tpl.php
@@ -1,11 +1,13 @@
 <?php if ($wrapper): ?><div<?php print $attributes; ?>><?php endif; ?>  
   <div<?php print $content_attributes; ?>>    
     <?php if ($breadcrumb): ?>
-      <div class="breadcrumb-wrapper" class="container-12">
-      <div class="grid-8 breadcrumb-left"> 
-        <?php print $breadcrumb; ?>
-      </div>
-      <div class="grid-4 breadcrumb-right">&nbsp;</div>
+      <div class="breadcrumb-wrapper">
+        <div class="grid-8 breadcrumb-left"> 
+          <?php print $breadcrumb; ?>
+        </div>
+        <div class="grid-4" id="breadcrumb-right-wrapper">
+          <div id="breadcrumb-right-inner">&nbsp;</div>
+        </div>
       </div>
     <?php endif; ?>    
     <?php if ($messages): ?>
