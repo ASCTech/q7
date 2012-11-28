4db602b6a455a9b5558fab7ba48cad90a48506a0
Mike Butsko <butsko.7@osu.edu>
2012-11-28 13:16:00 -0500

    typo

--- templates/zone--header.tpl.php
+++ templates/zone--header.tpl.php
@@ -5,8 +5,8 @@
   <div<?php print $content_attributes; ?>>    
     <?php if (($is_front) && (variable_get('q7_remove_asc_branding') != 1)) : ?>
       <div id="banner-container"><a id="ascbanner" href="http://asc.osu.edu/">College of Arts and Sciences</a></div>
-    <?php endif; ?>    
-    <?php if ((!$is_front) : ?>
+    <?php endif; ?>
+    <?php if (!$is_front) : ?>
       <div id="breadcrumb-extension-container">&nbsp;</div>
     <?php endif; ?>    
     <?php print $content; ?>
