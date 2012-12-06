f19e888894435aaa883f84f24ed25c7c24ca8cfc
Mike Butsko <butsko.7@osu.edu>
2012-12-06 16:05:32 -0500

    scroll to letter

---
+++ js/scrollletter.js
@@ -0,0 +1,16 @@
+Drupal.behaviors.myModuleBehavior = function (context) {
+  $("a.letter-select").click(function(){
+    var letter = $(this).html();
+    $("a.letter-select").css('color', '#000000');
+    $(this).css('color', '#990000')l
+    var firstItem = $("a." + letter).filter(':first');
+    var top = firstItem.offset().top;
+    $("body,html").animate({scrollTop: top - 75}, 400);
+  });
+
+  $("#backtotop").click(function(){
+    var top = $('body').top;
+    $("body,html").animate({scrollTop: top}, 400);
+  });
+
+};
