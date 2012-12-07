6e104fe94747983f7f706a5f2bdfc9a4b4f555fc
Mike Butsko <butsko.7@osu.edu>
2012-12-07 15:23:41 -0500

    hover color for most things?

--- js/scrollletter.js
+++ js/scrollletter.js
@@ -1,16 +1,16 @@
-Drupal.behaviors.myModuleBehavior = function (context) {
-  $("a.letter-select").click(function(){
+jQuery(function() {
+    jQuery("a.letter-select").click(function(){
     var letter = $(this).html();
-    $("a.letter-select").css('color', '#000000');
-    $(this).css('color', '#bb0000');
+    jQuery("a.letter-select").css('color', '#000000');
+    jQuery(this).css('color', '#bb0000');
     var firstItem = $("a." + letter).filter(':first');
     var top = firstItem.offset().top;
-    $("body,html").animate({scrollTop: top - 75}, 400);
+    jQuery("body,html").animate({scrollTop: top - 75}, 400);
   });
 
   $("#backtotop").click(function(){
     var top = $('body').top;
-    $("body,html").animate({scrollTop: top}, 400);
+    jQuery("body,html").animate({scrollTop: top}, 400);
   });
 
 };
