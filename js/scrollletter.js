667f2af47793c84b60bf3c8bdd2b2eae88e5e27d
Mike Butsko <butsko.7@osu.edu>
2012-12-07 15:25:01 -0500

    hover color for most things?

--- js/scrollletter.js
+++ js/scrollletter.js
@@ -1,4 +1,3 @@
-jQuery(function() {
     jQuery("a.letter-select").click(function(){
     var letter = $(this).html();
     jQuery("a.letter-select").css('color', '#000000');
@@ -7,10 +6,3 @@ jQuery(function() {
     var top = firstItem.offset().top;
     jQuery("body,html").animate({scrollTop: top - 75}, 400);
   });
-
-  $("#backtotop").click(function(){
-    var top = $('body').top;
-    jQuery("body,html").animate({scrollTop: top}, 400);
-  });
-
-};
