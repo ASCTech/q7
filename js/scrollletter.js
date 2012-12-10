811fbdf60d6abeb2ad0652fe2f19e12690955a88
Mike Butsko <butsko.7@osu.edu>
2012-12-10 09:00:30 -0500

    drupal 7 style javascript

--- js/scrollletter.js
+++ js/scrollletter.js
@@ -1,18 +1,19 @@
 (function ($) {
-  Drupal.behaviors.scollLetter = function (context) {
-    $("a.letter-select").click(function(){
-      var letter = $(this).html();
-      $("a.letter-select").css('color', '#000000')
-      $(this).css('color', '#990000')
-      var firstItem = $("a." + letter).filter(':first');
-    var top = firstItem.offset().top;
-    $("body,html").animate({scrollTop: top - 75}, 400);
-    });
+  Drupal.behaviors.scollLetter = {
+    attach: function (context) {
+              $("a.letter-select").click(function(){
+                var letter = $(this).html();
+                $("a.letter-select").css('color', '#000000')
+                $(this).css('color', '#990000')
+                var firstItem = $("a." + letter).filter(':first');
+              var top = firstItem.offset().top;
+              $("body,html").animate({scrollTop: top - 75}, 400);
+              });
 
-    $("#backtotop").click(function(){
-      var top = $('body').top;
-      $("body,html").animate({scrollTop: top}, 400);
-    });
-  };
+              $("#backtotop").click(function(){
+                var top = $('body').top;
+                $("body,html").animate({scrollTop: top}, 400);
+              });
+            };
 
-})(jQuery);
+  })(jQuery);
