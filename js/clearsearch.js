eacbab15adc9db48b161d2d217080356aa07aca3
Mike Butsko <butsko.7@osu.edu>
2012-12-11 11:02:38 -0500

    typo

---
+++ js/clearsearch.js
@@ -0,0 +1,32 @@
+(function ($) {
+  Drupal.behaviors.scollLetter = {
+    attach: $(function(){
+              var defaultText = "Search";
+
+              $('#edit-search-block-form--2').each(function(){
+                var $this = $(this);
+
+                var focusFunc = function(){
+                  $this.addClass("focused");
+                  if($this.val() == defaultText) $this.val("");
+                };
+                var blurFunc = function(){
+                  if(!$this.val()){
+                    $this.removeClass("focused");
+                    $this.val(defaultText);
+                  }
+                };
+
+                // Bind the functions
+                $this.focus(focusFunc).blur(blurFunc);
+
+                // Initialize the search bar without actually focusing it,
+                // because focusing scrolls the page to the element.
+                focusFunc();
+                blurFunc();
+              });
+            });
+
+  };
+
+})(jQuery);
