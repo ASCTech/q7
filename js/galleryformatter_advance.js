fd9de04c640dd704cb40f9d44eb87e1f18f5607d
Mike Butsko <butsko.7@osu.edu>
2012-12-11 14:23:49 -0500

    roll galleryformatter_advance into theme

---
+++ js/galleryformatter_advance.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function() {
+  var advance = setInterval("jQuery('a.next-slide').trigger('click')",3000);
+  jQuery('.galleryformatter').hover(
+     function () {
+       clearInterval(advance);
+     },
+     function () {
+       advance = setInterval("jQuery('a.next-slide').trigger('click')",3000);
+     }
+   );
+
+
+  var displayThumbs = jQuery('.field-name-field-displaythumbs').text();
+  if (displayThumbs=='0') {
+    jQuery('.gallery-thumbs').hide();
+  }
+}
+
+    );
