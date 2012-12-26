c0e3e8d2cdae993f24d48f4a2b42a78e82ead475
Mike Butsko <butsko.7@osu.edu>
2012-12-26 15:42:40 -0500

    active row javascript

---
+++ js/active_row.js
@@ -0,0 +1,7 @@
+(function ($) {
+  Drupal.behaviors.scollLetter = {
+    attach: $(function() {
+              $('.views-table a.active').closest('tr').addClass('active_row');
+            });
+  };
+})(jQuery);
