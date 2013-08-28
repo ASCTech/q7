a704fc44c4156491c7112b629c0815ea37c8c860
Dustin Perzanowski <perzanowski.4@osu.edu>
2013-08-28 13:59:16 -0400

    Add code to make copyright date always be the current year.

--- templates/section--footer.tpl.php
+++ templates/section--footer.tpl.php
@@ -2,7 +2,7 @@
 <?php print $content; ?>
 <div class="container-12">
   <div id="copyright" class="grid-12">
-    &copy; 2012, The Ohio State University, Designed by·
+    &copy; <?php echo date('Y') ?>, The Ohio State University, Designed by · 
     <a href="http://asccomm.osu.edu" title="ASC Communications website">Arts and Sciences Communications Services</a>.
     <?php if(!user_is_logged_in()) { ?>·
     <a href="<?php print shib_auth_generate_login_url(); ?>" title="Site Administrator Login"><img src="<?php print base_path() . drupal_get_path('theme', 'q7') . '/images/user.gif' ?>" alt="Log In" class="user-auth-btn" /></a>
