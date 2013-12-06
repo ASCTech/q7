64c65fb4f599a8ae0337d5cc928269d9db482ccb
Dustin Perzanowski <perzanowski.4@osu.edu>
2013-12-06 11:16:07 -0500

    update skip link in html.tpl.php

--- html.tpl.php
+++ html.tpl.php
@@ -9,7 +9,7 @@
 </head>
 <body<?php print $attributes;?>>
   <div id="skip-link">
-    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+    <a href="#zone-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
   </div>
   <?php print $page_top; ?>
   <?php print $page; ?>
