cab1de78e0eae05c745a19e67b463f2433f2a50a
Dustin Perzanowski <perzanowski.4@osu.edu>
2014-07-03 15:22:45 -0400

    fix typo in wordmark URL

--- templates/region--footer_first.tpl.php
+++ templates/region--footer_first.tpl.php
@@ -1,7 +1,7 @@
 <div<?php print $attributes; ?>>
   <div<?php print $content_attributes; ?>>
 		<?php $theme_path = drupal_get_path('theme',$GLOBALS['theme']) ?>
-    <img src="<?php echo $theme_path ?>/images/d7-footer-wordmark-270px.png" width="270" height="24" alt="The Ohio State University">
+    <img src="/<?php echo $theme_path ?>/images/d7-footer-wordmark-270px.png" width="270" height="24" alt="The Ohio State University">
     <?php print $content; ?>
   </div>
 </div>
